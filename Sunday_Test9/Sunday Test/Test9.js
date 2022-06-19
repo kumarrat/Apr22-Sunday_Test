9acb7f42baba081e52c083a134ef6590ac893cee
unknown <rockratnsh@gmail.com>
2022-06-19 14:50:29 +0530

    Compleated Sunday Test9

---
+++ Sunday_Test9/Sunday Test/Test9.js
@@ -0,0 +1,63 @@
+//1.An array is given, return the sum of all the positives numbers. const input = [ 1, -5, 2, 10, -30, 29, 50];
+
+//const arr = [ 1, 2, -5, 10, -30, 29, 50];
+
+//let l = arr.length
+//let sum = 0
+
+//function sumOfPositiveNumber() {
+
+//for (i=0; i<=arr.length - 1; i++) {
+
+// if (arr[i]>0) {
+//     sum = sum + arr[i]
+// }
+// }
+//console.log(sum);
+//}
+
+//sumOfPositiveNumber()
+
+//2. Multiple records of people are given, you have to print the first name and last name . let friends = [           {fname: 'Rachal', lname:"Green" ,id: 1, marks : 98 },          {fname: 'Ross',lname:"Geller" , id: 2, marks : 23 },          {fname: 'Monica ',lname:"Geller" , id: 3, marks : 75 },          {fname: 'Joey',lname:" Tribbiani" , id: 4, marks : 46 },          {fname: 'Chandler  ',lname:"Bing" , id: 5, marks : 78 },          {fname: 'Phoebe',lname:"Buffay" , id: 6, marks : 45 }           ]
+
+//let friends = [
+ // { fname: "Rachal", lname: "Green", id: 1, marks: 98 },
+ // { fname: "Ross", lname: "Geller", id: 2, marks: 23 },
+ // { fname: "Monica ", lname: "Geller", id: 3, marks: 75 },
+ // { fname: "Joey", lname: " Tribbiani", id: 4, marks: 46 },
+ // { fname: "Chandler  ", lname: "Bing", id: 5, marks: 78 },
+ // { fname: "Phoebe", lname: "Buffay", id: 6, marks: 45 },
+//];
+
+//console.log(friends[0].fname);
+//console.log(friends[0].lname);
+
+//console.log(friends[1].fname);
+//console.log(friends[1].lname);
+
+//console.log(friends[2].fname);
+//console.log(friends[2].lname);
+
+//console.log(friends[3].fname);
+//console.log(friends[3].lname);
+
+//console.log(friends[4].fname);
+//console.log(friends[4].lname);
+
+//console.log(friends[5].fname);
+//console.log(friends[5].lname);
+
+//3.You are given some names of different cities and a ordered list html element; add a list item for each to the ordered list.   city = ['Delhi', 'Mumbai', 'Jaipur','Kolkata','Indore' ]; (below html code and output is given)
+
+//<h2 id="output"></h2>
+//var city = ['Delhi', 'Mumbai', 'Jaipur','Kolkata','Indore' ];
+
+//var display = '<OL>';
+
+//for (var i=0; i<city.length; i++) {
+   // display += "<li>" + flower[i] + "</li>";
+//}
+
+//display += "</ol>";
+
+//document.getElementById("output").innerHTML + display;
